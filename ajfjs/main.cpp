6804d8c15500089006cdd22cb1784f053186e216
rumeysaacelikk <152120211125@ogrenci.ogu.edu.tr>
2024-09-25 13:33:23 +0300

    Initial Commit

---
+++ ajfjs/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ajfjs
+//
+//  Created by rümeysa çelik on 25.09.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+
